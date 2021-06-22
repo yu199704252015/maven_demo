d43470d61b650af7cc998092c9a3244b28c55c3f
“xunzhang” <1452356370@qq.com>
2021-06-22 19:34:15 +0800

    first commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 勋章
+ * @Author: xunzhang
+ * @Create: 2021-06-22 19:14
+ * @Desc：
+ **/
+public class LoginTest {
+    @Test
+    public void loginSuccess(){
+        System.out.println("登陆成功");
+    }
+}
---
+++ src/test/java/testcases/RegisterTest.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 勋章
+ * @Author: xunzhang
+ * @Create: 2021-06-22 19:14
+ * @Desc：
+ **/
+public class RegisterTest {
+    @Test
+    public void registerSuccess(){
+        System.out.println("注册成功");
+    }
+}
